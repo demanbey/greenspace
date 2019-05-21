7a9b511f8fc78b8b6e83b899d29a6da9eb088129
Alex Tang <altg514@gmail.com>
2019-05-21 10:12:59 -0400

    Added in the users controller with all 7 typical methods - might need to update the paths for the create / update methods and for the permitted parameters under the find_user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,49 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
+    @users = User.all
   end
 
   def show
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to '/home' # Edit if neccessary
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to user_path(@user) # Edit if neccessary
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @user.destroy
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    # Permit as needed
+    params.require(:user).permit(:email, :first_name, :last_name, :description, :photo, :encrypted_password)
   end
 end
