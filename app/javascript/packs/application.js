529cc4a53be08b9b50d1adf9380db0202648b9f5
Alex Tang <altg514@gmail.com>
2019-05-22 11:21:35 -0400

    Added completed migration, button to create a booking and booking logic

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,13 @@
 import "bootstrap";
 import { initMapbox } from "../plugins/init_mapbox";
 import 'mapbox-gl/dist/mapbox-gl.css';
-import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+// import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 initMapbox();
+
+const clicked = document.querySelector("#participate-btn")
+if (clicked !== null) {
+  clicked.addEventListener("click", (event) => {
+    clicked.innerHTML = "Booked!"
+  })
+}
